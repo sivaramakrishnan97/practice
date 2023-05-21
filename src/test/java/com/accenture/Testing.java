d8b91f5d555bbfcfdbfa531317f6cfdbecd69080
sivaramakrishnan97 <sivaraman.drs@gmail.com>
2023-05-21 11:39:48 +0530

    21/5/23

--- src/test/java/com/accenture/Testing.java
+++ src/test/java/com/accenture/Testing.java
@@ -1,10 +1,37 @@
 package com.accenture;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Testing {
 	public static void main(String[] args) {
 		
+		WebDriverManager.chromedriver().setup();
 		
-		System.out.println("hello IT");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
